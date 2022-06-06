d6700c26b160112ad2ad9c83a816c8bf9b18e602
Juan <105522569+borianijc@users.noreply.github.com>
2022-06-06 09:59:14 -0300

    Add files via upload

---
+++ ing2s.py
@@ -0,0 +1,10 @@
+def ing2s():
+    cadena_1=input(str("Ingrese el primer comentario: "))
+    cadena_2=input(str("Ingrese el segundo comentario: "))
+    print(cadena_1,cadena_2)
+ing2s ()
+
+def prueba(a,b):
+    a,b=input(str("Ingrese el primer comentario: ")),input(str("Ingrese el segundo comentario: "))
+
+prueba()
